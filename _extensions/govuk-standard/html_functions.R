0a46c4be80e3e42865d1f7cb6de414e0c25f9b0d
Olivia Box Power <Olivia.BoxPower@dhsc.gov.uk>
2023-11-10 15:24:47 +0000

    Updates to GOVUK standard template following review

--- _extensions/govuk-standard/html_functions.R
+++ _extensions/govuk-standard/html_functions.R
@@ -1,11 +1,11 @@
-#' HTML drop down
+#' HTML details component
 #'
-#' `drop_down` produces HTML code for a details summary drop down
+#' `add_details` produces HTML code for a details summary drop down
 #'
 #' @param title character string, current topic
 #' @param description vector, list of all topic names
 
-drop_down <- function(title, description) {
+add_details <- function(title, description) {
 
   html1 <- '<html><link rel="stylesheet" href="./_extensions/govuk-standard/GDS.css"><details class="govuk-details" data-module="govuk-details"><summary class="govuk-details__summary"><span class="govuk-details__summary-text">'
 
@@ -14,4 +14,5 @@ drop_down <- function(title, description) {
   html3 <- '</div></details></html>'
 
   cat(paste0(html1, title, html2, description, html3))
+
 }
